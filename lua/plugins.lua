427f085180e53a2de644e2e5976394e3af348ff4
Frederico Plotze <plotze.frederico@gmail.com>
2023-10-13 09:58:28 -0300

    feature: add more plugins (such as shade, nvim-tree, statusline)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,10 +3,19 @@ require('packer').startup(function()
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
+  use 'sunjon/shade.nvim'
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip'
   use 'marko-cerovac/material.nvim'
   use 'Bekaboo/deadcolumn.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-lualine/lualine.nvim'
+  use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons'
+        }
+    }
   use {
         'nvim-treesitter/nvim-treesitter',
         run = function()
